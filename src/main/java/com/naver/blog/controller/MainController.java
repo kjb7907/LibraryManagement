1d22642392d42a85a83cdbc9992d386b577fc619
SMART20107\Administrator <kjb7907@naver.com>
2017-01-25 11:41:41 +0900

    main 컨트롤러 / 페이지 작업전

---
+++ src/main/java/com/naver/blog/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.naver.blog.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+
+	@RequestMapping(value = "main", method = RequestMethod.GET)
+	public String main(){
+		return "main";
+	}
+	
+}
